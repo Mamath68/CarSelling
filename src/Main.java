5a1785f8a6dbe44dfcfde71a51d83ad8474d229b
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2025-01-17 14:42:50 +0100

    création class Cars

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+/*
+ * Votre tâche est de créer un système de commande de voiture.
+ * Un client doit pouvoir choisir différentes options pour sa voiture.
+ * Les options sont en Enum et non en String.
+ * Créer au moins DEUX exemples de voiture avec le Director, et un exemple libre. Afficher le contenu via un toString.
+ */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
